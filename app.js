2114da84273c79d80ae9b54ce1599b5779d4fc03
MOHAMMED TAHER <100471051+samhamed@users.noreply.github.com>
2023-09-12 18:56:25 +0100

    12/09/2023

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const v1 = require("./routes");
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
+const fs = require("fs");
+
+const requestLogger = (req, res, next) => {
+  const logEntry = `${new Date().toISOString()}: ${req.method} ${req.url}\n`;
+  fs.appendFile("requestLog.txt", logEntry, (err) => {
+    if (err) {
+      console.error("Error logging request:", err);
+    }
+  });
+  next();
+};
+app.use(requestLogger);
 
 app.use("/v1", v1);
 const port = process.env.PORT ?? 4000;
